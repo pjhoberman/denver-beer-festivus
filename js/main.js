f3637f9600d405d5142f0dc6800913f15cc55de9
PJ Hoberman <pj.hoberman@gmail.com>
2012-10-23 23:22:39 -0600

    base content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){
+    $('nav li').on('click', function () {
+        var _this = this;
+        $('section').hide();
+        $('section#' + $(_this).attr('section')).show();
 
+    });
+})
