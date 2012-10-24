ee6c735c6a1eb79bbbfbb2e8c6b1a0ee3a62c772
PJ Hoberman <pj.hoberman@gmail.com>
2012-10-23 23:50:28 -0600

    js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
+    // nav clicking
     $('nav li').on('click', function () {
         var _this = this;
         $('section').hide();
-        $('section#' + $(_this).attr('section')).show();
-
+        $('section#' + $(_this).attr('data-section')).show();
+        window.location.hash = $(_this).attr('data-section');
     });
+
+    // url hashing
+    if (window.location.hash != '') {
+        var hash = window.location.hash.replace('#', '');
+        if ($('li[data-section=' + hash + ']').length) {
+            $('li[data-section=' + hash + ']').click();
+        } else {
+            $('li[data-section=home]').click();
+        }
+
+    }
 })
